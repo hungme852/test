6ceb98453483eb09feee059b019715505b6e3092
Võ Văn Hùng <hungme12@yahoo.com>
2016-06-10 09:32:06 +0700

    the

--- ConsoleApplication1/ConsoleApplication1/Properties/AssemblyInfo.cs
+++ ConsoleApplication1/ConsoleApplication1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AppTest")]
+[assembly: AssemblyTitle("ConsoleApplication1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AppTest")]
+[assembly: AssemblyProduct("ConsoleApplication1")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a61ac6cb-7307-4e1e-a0e9-312d1aaad834")]
+[assembly: Guid("5d825ada-b612-4257-80b6-6703091e99d4")]
 
 // Version information for an assembly consists of the following four values:
 //
